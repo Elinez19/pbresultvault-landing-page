8193f6c29d76e8b19f90333eecf6acd11b980b05
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-01-23 22:03:19 +0100

    pbresult-landing-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import FeatureCards from "@/components/FeatureCards";
 import FeatureLists from "@/components/FeatureLists";
+import FeaturesOne from "@/components/FeaturesOne";
 import Hero from "@/components/Hero";
 import { Metadata } from "next";
 
@@ -18,6 +19,7 @@ export default function Home() {
       <Hero />
       <FeatureCards />
       <FeatureLists />
+      <FeaturesOne />
     </main>
   );
 }
--- components/FeatureLists.tsx
+++ components/FeatureLists.tsx
@@ -1,107 +1,88 @@
 import Image from "next/image";
+
 import React from "react";
 
 const FeatureLists = () => {
   return (
-    <section className="overflow-hidden bg-white py-8 sm:py-16">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Produce faster
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                An Enterprise API
-              </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                We have built an API that allows you to scale your podcast
-                production workflow.
-              </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                <div className="relative pl-9">
-                  <dt className="inline font-semibold text-gray-900">
+    <section className="mb-20">
+      <div className="max-w-xl mt-30 mx-auto space-y-12 lg:px-8 lg:max-w-7xl">
+        <div className="">
+          <h2 className="text-5xl font-bold text-center sm:text-5xl">
+            30+ Powerful Amazing Result Features.
+          </h2>
+          <p className="max-w-3xl mx-auto mt-4 text-xl text-center ">
+            PBResultVault is a completely full-featured result management system
+            that meet your school needs. With over 30+ features and more still
+            will be added as the need arises. We are sure of giving your school
+            a stress free result reporting system.
+          </p>
+        </div>
+        <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+          <div>
+            <div className="mt-4 space-y-12">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark-bg-emerald-400 dark-text-gray-900">
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
+                      className="w-45 h-45 text-indigo-600"
                       fill="currentColor"
-                      aria-hidden="true"
-                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600">
-                      <path d="M3.196 12.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 12.87z"></path>
-                      <path d="M3.196 8.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 8.87z"></path>
-                      <path d="M10.38 1.103a.75.75 0 00-.76 0l-7.25 4.25a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.76 0l7.25-4.25a.75.75 0 000-1.294l-7.25-4.25z"></path>
-                    </svg>
-                    Template driven
-                  </dt>
-                  <dd className="inline">
-                    Inspired by Sendgrid, Mailchimp, and Postmark, we allow you
-                    to create and apply templated content to your media.
-                  </dd>
-                </div>
-                <div className="relative pl-9">
-                  <dt className="inline font-semibold text-gray-900">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600">
+                      viewBox="0 0 20 20">
                       <path
                         fill-rule="evenodd"
-                        d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                         clip-rule="evenodd"></path>
                     </svg>
-                    Simple, REST
-                  </dt>
-                  <dd className="inline">
-                    A simple REST API that allows you to create, generate, and
-                    manage your content.
-                  </dd>
+                  </div>
                 </div>
-                <div className="relative pl-9">
-                  <dt className="inline font-semibold text-gray-900">
+                <div className="ml-4">
+                  <h4 className="text-3xl font-medium leading-tight ">
+                    Manage & Push Your School Result To The Next Level
+                  </h4>
+                  <p className="mt-2 ">
+                    Cut down the time taken for exam evaluations, mark
+                    calculation, and report card generation by 90% using
+                    PBResultVault. PBResultVault gives flexibility and clarity
+                    to your school result regardless the result format.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark-bg-emerald-400 dark-text-gray-900">
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
+                      className="w-45 h-45 text-indigo-600"
                       fill="currentColor"
-                      aria-hidden="true"
-                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600">
+                      viewBox="0 0 20 20">
                       <path
                         fill-rule="evenodd"
-                        d="M14.5 10a4.5 4.5 0 004.284-5.882c-.105-.324-.51-.391-.752-.15L15.34 6.66a.454.454 0 01-.493.11 3.01 3.01 0 01-1.618-1.616.455.455 0 01.11-.494l2.694-2.692c.24-.241.174-.647-.15-.752a4.5 4.5 0 00-5.873 4.575c.055.873-.128 1.808-.8 2.368l-7.23 6.024a2.724 2.724 0 103.837 3.837l6.024-7.23c.56-.672 1.495-.855 2.368-.8.096.007.193.01.291.01zM5 16a1 1 0 11-2 0 1 1 0 012 0z"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                         clip-rule="evenodd"></path>
-                      <path d="M14.5 11.5c.173 0 .345-.007.514-.022l3.754 3.754a2.5 2.5 0 01-3.536 3.536l-4.41-4.41 2.172-2.607c.052-.063.147-.138.342-.196.202-.06.469-.087.777-.067.128.008.257.012.387.012zM6 4.586l2.33 2.33a.452.452 0 01-.08.09L6.8 8.214 4.586 6H3.309a.5.5 0 01-.447-.276l-1.7-3.402a.5.5 0 01.093-.577l.49-.49a.5.5 0 01.577-.094l3.402 1.7A.5.5 0 016 3.31v1.277z"></path>
                     </svg>
-                    Developer friendly{" "}
-                  </dt>
-                  <dd className="inline">
-                    Documented and easy to use, we make it easy to integrate
-                    with your existing workflow.
-                  </dd>
+                  </div>
                 </div>
-              </dl>
-            </div>
-            <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Start for free
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-700">
-                Schedule a demo
-                <span aria-hidden="true">→</span>
-              </a>
+                <div className="ml-4">
+                  <h4 className="text-3xl font-medium leadi ">
+                    Advanced Quiz Generation{" "}
+                  </h4>
+                  <p className="mt-2 ">
+                    Take your knowledge testing to the next level with advanced
+                    quiz generation, providing more customization options for
+                    your quizzes.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-          <Image
-            src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb21wdXRlcnxlbnwwfDB8fHwxNjkxODE2NjY3fDA&ixlib=rb-4.0.3&q=80&w=1080"
-            alt="Product screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-            width="2432"
-            height="1442"
-          />
+
+          <div aria-hidden="true" className="mt-10 lg:mt-0">
+            <Image
+              alt=""
+              width="800"
+              height="800"
+              src="/pbresult-hero-2.png"
+              className="mx-auto rounded-lg shadow-lg dark-bg-gray-500 "
+            />
+          </div>
         </div>
       </div>
     </section>
